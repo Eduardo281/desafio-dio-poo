71de88aed62e49371e21154afda478f7153898d2
Eduardo <edteixeira28@hotmail.com>
2022-12-01 11:06:16 -0300

    Adição da classe Link

---
+++ src/br/com/dio/desafio/dominio/Link.java
@@ -0,0 +1,22 @@
+package br.com.dio.desafio.dominio;
+
+public class Link {
+    private String titulo;
+    private String url;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
